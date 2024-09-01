9b76fac32fd291f172c087b6dcda293aaa3e48a1
Sherif-lotfy <s1h9e7r4if@gmail.com>
2024-09-02 01:58:07 +0300

    fixing some issues

--- src/app/plan/page.js
+++ src/app/plan/page.js
@@ -5,7 +5,7 @@ export default function Plans() {
   return (
     <>
       <NavBar currentPage={'plan'}/>
-      <div className="grid grid-cols-3 gap-6 justify-items-center">
+      <div className="grid grid-cols-3 gap-6 justify-items-center pt-8">
         <Plan title={'plan 1'} describtion={'plan1 desc'} price={'20$'}/>
         <Plan title={'plan 2'} describtion={'plan2 desc'} price={'50$'}/>
         <Plan title={'plan 3'} describtion={'plan3 desc'} price={'60$'}/>
--- src/app/plan/plan.js
+++ src/app/plan/plan.js
@@ -1,16 +1,3 @@
-// export default function Plan({ title, describtion, price }) {
-//   return (
-//     <div className=" rounded-md mt-20 hover:mt-16 border-blue-900 border-2 flex items-center w-44 h-80 transition-all flex-col justify-between p-5">
-//       <div className="flex items-start flex-col w-full">
-//         <h2>{title}</h2>
-//         <p>{describtion}</p>
-//       </div>
-//       <div className="flex flex-col items-end  w-full">{price}</div>
-//     </div>
-//   );
-// }
-// import React from "react";
-
 const Plan = ({ title, describtion, price }) => {
   return (
     <div className="flex flex-col bg-white rounded-3xl">
@@ -18,16 +5,14 @@ const Plan = ({ title, describtion, price }) => {
         <div className="grid items-center justify-center w-full grid-cols-1 text-left">
           <div>
             <h2 className="text-lg font-medium tracking-tighter text-gray-600 lg:text-3xl">
-            {title}
+              {title}
             </h2>
-            <p className="mt-2 text-sm text-gray-500">
-            {describtion}
-            </p>
+            <p className="mt-2 text-sm text-gray-500">{describtion}</p>
           </div>
           <div className="mt-6">
             <p>
               <span className="text-5xl font-light tracking-tight text-black">
-              {price}
+                {price}
               </span>
               <span className="text-base font-medium text-gray-500"> /mo </span>
             </p>
@@ -38,7 +23,7 @@ const Plan = ({ title, describtion, price }) => {
         <a
           aria-describedby="tier-company"
           className="flex items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full nline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black"
-          href="#"
+          href="./signup"
         >
           Get started
         </a>
@@ -47,4 +32,4 @@ const Plan = ({ title, describtion, price }) => {
   );
 };
 
-export default Card;
+export default Plan;
