0d61af5095edabc683820f000187711ddde49335
Sherif <127263385+sizif-22@users.noreply.github.com>
2024-09-12 14:01:31 +0300

    removing the session storage

--- src/app/page.js
+++ src/app/page.js
@@ -9,61 +9,58 @@ import Overview from "./HomePage body/overview";
 
 export const userData = createContext();
 const Home = () => {
-  const [userState, setUserState] = useState({});
-  const [loading, setLoading] = useState(true);
+  // const [userState, setUserState] = useState({});
 
   //this functoin has to be removed ...
-  const fetchUserData = async () => {
-    const isLoggedIn = await checkLoggedIn();
-    const isVerified = await checkVerified();
+  // const fetchUserData = async () => {
+  //   const isLoggedIn = await checkLoggedIn();
+  //   const isVerified = await checkVerified();
 
-    if (typeof window !== "undefined" && isLoggedIn) {
-      const userData = await getUser();
-      const user = userData.data();
+  //   if (typeof window !== "undefined" && isLoggedIn) {
+  //     const userData = await getUser();
+  //     const user = userData.data();
 
-      const newUserState = {
-        isLoggedIn,
-        isVerified,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        photoUrl: user.photoUrl,
-        companyName: user.companyName,
-        accountType: user.accountType,
-      };
-      const sessionData = sessionStorage.getItem("userState");
-      if (!sessionData || JSON.stringify(newUserState) !== sessionData) {
-        setUserState(newUserState);
-        sessionStorage.setItem("userState", JSON.stringify(newUserState));
-      }
-    }
-    setLoading(false);
-  };
+  //     const newUserState = {
+  //       isLoggedIn,
+  //       isVerified,
+  //       email: user.email,
+  //       firstName: user.firstName,
+  //       lastName: user.lastName,
+  //       photoUrl: user.photoUrl,
+  //       companyName: user.companyName,
+  //       accountType: user.accountType,
+  //     };
+  //     const sessionData = sessionStorage.getItem("userState");
+  //     if (!sessionData || JSON.stringify(newUserState) !== sessionData) {
+  //       setUserState(newUserState);
+  //       sessionStorage.setItem("userState", JSON.stringify(newUserState));
+  //     }
+  //   }
+  //   setLoading(false);
+  // };
 
-  useEffect(() => {
-    if (sessionStorage.getItem("userState")) {
-      const data = JSON.parse(sessionStorage.getItem("userState"));
-      setUserState(data);
-      setLoading(false);
-    } else {
-      fetchUserData();
-    }
-    const intervalId = setInterval(() => {
-      fetchUserData();
-    }, 20000);
+  // useEffect(() => {
+  //   if (sessionStorage.getItem("userState")) {
+  //     const data = JSON.parse(sessionStorage.getItem("userState"));
+  //     setUserState(data);
+  //     setLoading(false);
+  //   } else {
+  //     fetchUserData();
+  //   }
+  //   const intervalId = setInterval(() => {
+  //     fetchUserData();
+  //   }, 20000);
 
-    return () => clearInterval(intervalId);
-  }, []);
+  //   return () => clearInterval(intervalId);
+  // }, []);
 
-  return loading ? (
-    <Loading />
-  ) : (
+  return (
     <div className="flex flex-col min-h-screen bg-gray-100 text-gray-800">
       <div className="flex-1 flex flex-col lg:flex-row">
         <div className="flex-1 flex flex-col">
-          <userData.Provider value={userState}>
+          {/* <userData.Provider value={userState}> */}
             <NavBar className="bg-blue-600 text-white" />
-          </userData.Provider>
+          {/* </userData.Provider> */}
           <main className="flex-1 p-6 bg-white">
             <div className="ml-auto mr-auto bg-gray-500 h-screen max-w-6xl">
               <Overview />
--- src/app/plan/page.js
+++ src/app/plan/page.js
@@ -1,62 +1,8 @@
 "use client";
-import Loading from "../components/loading/loading";
-import NavBar from "../components/nav";
-import { checkLoggedIn ,checkVerified } from "../firebase/firebase.auth";
 import Plan from "./plan";
-import { useState, useEffect, createContext } from "react";
-
-export const userDataPlan = createContext();
-
 export default function Plans() {
-  const [userState, setUserState] = useState(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (sessionStorage.getItem("userState")) {
-      const data = JSON.parse(sessionStorage.getItem("userState"));
-      setUserState(data); // Set the state
-      setLoading(false);
-    } else {
-      const checkAuth = async () => {
-        const isLoggedIn = await checkLoggedIn();
-        const isVerified = await checkVerified();
-
-        if (typeof window !== "undefined" && isLoggedIn) {
-          const userData = await getUser();
-          const user = userData.data();
-
-          const newUserState = {
-            isLoggedIn,
-            isVerified,
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            photoUrl: user.photoUrl,
-            companyName: user.companyName,
-            accountType: user.accountType,
-          };
-
-          setUserState(newUserState);
-
-          sessionStorage.setItem("userState", JSON.stringify(newUserState));
-        }
-        setLoading(false);
-      };
-
-      checkAuth();
-    }
-  }, []);
-
-  // Log userState when it changes
-  useEffect(() => {
-    if (userState) {
-    }
-  }, [userState]);
-
-  return loading ? (
-    <Loading />
-  ) : (
-    <userDataPlan.Provider value={userState}> 
+  return(
       <div className="h-screen">
         <div className="plans-page p-6 bg-gray-100 min-h-screen">
           <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">
@@ -81,6 +27,5 @@ export default function Plans() {
           </div>
         </div>
       </div>
-    </userDataPlan.Provider>
   );
 }
