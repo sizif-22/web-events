94ecddccfbc3f200f777842b4ccb67b3b30b8b2f
Sherif <127263385+sizif-22@users.noreply.github.com>
2025-03-25 19:27:54 +0200

    update the loading page

--- src/app/components/loading/loading.js
+++ src/app/components/loading/loading.js
@@ -1,23 +1,19 @@
-import React from "react";
-import "./loading.css";
+import { Zoomies } from "ldrs/react";
+import "ldrs/react/Zoomies.css";
 
 export default function Loading() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="spinner center">
-        <div className="spinner-blade" />
-        <div className="spinner-blade" />
-        <div className="spinner-blade" />
-        <div className="spinner-blade" />
-        <div className="spinner-blade" />
-        <div className="spinner-blade" />
-        <div className="spinner-blade" />
-        <div className="spinner-blade" />
-        <div className="spinner-blade" />
-        <div className="spinner-blade" />
-        <div className="spinner-blade" />
-        <div className="spinner-blade" />
-      </div>
+    <div className="h-screen flex justify-center flex-col items-center bg-black gap-3 text-white">
+      <h1 className="text-2xl md:text-4xl select-none font-[Italiana]">
+        Webbing Events
+      </h1>
+      <Zoomies
+        size="100"
+        stroke="5"
+        bgOpacity="0.1"
+        speed="1.4"
+        color="white"
+      />
     </div>
   );
 }
